5ccbc6ae5a5eed285ca41507f2841c55de04bf1c
Lucas Bailo <lucasebailo@gmail.com>
2023-06-03 15:30:32 -0300

    home page - test

---
+++ main.js
@@ -0,0 +1,12 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') {
+        event.preventDefault()
+    }
+    const navi = document.getElementById('nav');
+    navi.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
