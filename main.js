3eac2d3b424c9266f7823fb861d29680d2ef6bb0
Lucas Bailo <lucasebailo@gmail.com>
2023-06-20 15:43:21 -0300

    creating animated menu in mobile

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// const btnMobile = document.getElementById('btn-mobile');
+
+// function toggleMenu(event) {
+//     if(event.type === 'touchstart') {
+//         event.preventDefault()
+//     }
+//     const navi = document.getElementById('nav');
+//     navi.classList.toggle('active')
+// }
+
+// btnMobile.addEventListener('click', toggleMenu);
+// btnMobile.addEventListener('touchstart', toggleMenu);
+
+
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
