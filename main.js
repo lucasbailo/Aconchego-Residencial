9a5e025d35503138f7fd772b4d15091175972fc7
Lucas Bailo <lucasebailo@gmail.com>
2023-06-25 15:21:20 -0300

    changing footer size

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// const btnMobile = document.getElementById('btn-mobile');
-
-// function toggleMenu(event) {
-//     if(event.type === 'touchstart') {
-//         event.preventDefault()
-//     }
-//     const navi = document.getElementById('nav');
-//     navi.classList.toggle('active')
-// }
-
-// btnMobile.addEventListener('click', toggleMenu);
-// btnMobile.addEventListener('touchstart', toggleMenu);
-
-
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
