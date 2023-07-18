7070a464a9cc02f5ceb4d5e52019bc99434010ef
Lucas Bailo <lucasebailo@gmail.com>
2023-07-17 21:37:58 -0300

    adding a code to close menu hamburguer when user click any part of the screen but the menu

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const btnMobile = document.getElementById('btn-mobile');
 
+//abrir e fechar o menu hamburguer quando o usuário clicar nele - open and close menu hamburguer when user click on it
 function toggleMenu(event) {
     if(event.type === 'touchstart') {
         event.preventDefault()
     }
     const navi = document.getElementById('nav');
-    navi.classList.toggle('active')
+    navi.classList.toggle('active');
+}
+
+//fechar o menu quando o usuário clicar fora do menu hamburguer - close menu when user click outside off hamburguer menu
+function clickOut(event) {
+    const navi = document.getElementById('nav');
+    if(event.target != navi) {
+        navi.classList.remove('active');
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+document.addEventListener('click', clickOut)
+
+
